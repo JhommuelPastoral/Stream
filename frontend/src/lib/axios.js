cdf8e0c12ec6be4e27db1bddb2c2e14673905892
JhommuelPastoral <jhommuelpastoral9@gmail.com>
2025-05-11 13:13:48 +0800

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://streambackend-ov4o.onrender.com/api',
+  baseURL: 'https://streambackend-fvop.onrender.com/api',
   withCredentials: true
 });
 
